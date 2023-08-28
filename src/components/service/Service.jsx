ac4912891b07607d546189a46d699a780c39505a
kervinazo <kervinazo3@gmail.com>
2023-08-28 12:14:02 -0400

    firts commit

---
+++ src/components/service/Service.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import './service.css'
+import {FiMenu}  from 'react-icons/fi';
+
+const Service = () => {
+  return (
+    <section id="service">
+        <h5 className="service__title-primary">What I Offer</h5>
+        <h2 className="service__title">services</h2>
+        <div className="container service__container">
+            <article className="service">
+                <div className="service__head">
+                    <h3>servicio de cambio </h3>
+                </div>
+                <ul className="service__list">
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                </ul>
+            </article>
+            
+            <article className="service">
+                <div className="service__head">
+                    <h3>servicio de cambio</h3>
+                </div>
+                <ul className="service__list">
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                </ul>
+            </article>
+            <article className="service">
+                <div className="service__head">
+                    <h3>servicio de cambio </h3>
+                </div>
+                <ul className="service__list">
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                    <li>
+                        <i className="uil uil-check icons"></i>
+                        <p>lorem, ipsum dolor sit amet conccs asdkiandlanla ksandalnadkl.</p>
+                    </li>
+                </ul>
+            </article>
+        </div>
+    </section>
+  )
+}
+
+export default Service
